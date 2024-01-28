dc0fe6ed9fd2bf5497737809a657a180efe0f3e0
Mo'men Mohamed <84360276+momen882001@users.noreply.github.com>
2024-01-28 03:21:36 +0200

    Structure + player movements

---
+++ Assets/Scripts/PlayerLook.cs
@@ -0,0 +1,24 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerLook : MonoBehaviour
+{
+    public Camera cam;
+    private float xRotation = 0f;
+    public float xSenstivity = 30f;
+    public float ySenstivity = 30f;
+
+    public void ProcessLook(Vector2 input)
+    {
+        float mouseX = input.x;
+        float mouseY = input.y;
+
+        xRotation -= mouseY * Time.deltaTime * ySenstivity;
+        xRotation = Mathf.Clamp(xRotation , -80f , 80f);
+
+        cam.transform.localRotation = Quaternion.Euler(xRotation , 0 , 0);
+        transform.Rotate(Vector3.up * ( mouseX * Time.deltaTime ) * xSenstivity); 
+    }
+}
