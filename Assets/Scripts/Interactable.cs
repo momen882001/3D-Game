7ba65c3f3a0ec8b8fe92fec0bffcac3d494c43eb
Zeyad Amr <68791488+Zeyad-Amr@users.noreply.github.com>
2024-02-03 04:37:11 +0300

    Add useEvents in Interactable script

--- Assets/Scripts/Interactable.cs
+++ Assets/Scripts/Interactable.cs
@@ -4,12 +4,18 @@ using UnityEngine;
 
 public abstract class Interactable : MonoBehaviour
 {
+    public bool useEvents;
     // message displayed to player when looking at interactable
+    [SerializeField]
     public string promptMessage;
 
     // this function will be called from our player
     public void BaseInteract()
     {
+        if (useEvents)
+        {
+            GetComponent<InteractionEvent>().onInteract.Invoke();
+        }
         Interact();
     }
 
--- Assets/Scripts/Player/PlayerUI.cs
+++ Assets/Scripts/Player/PlayerUI.cs
@@ -6,11 +6,6 @@ using TMPro;
 public class PlayerUI : MonoBehaviour
 {
     [SerializeField] private TextMeshProUGUI promptText;
-    // Start is called before the first frame update
-    void Start()
-    {
-
-    }
 
     // Update is called once per frame
     public void UpdateText(string promptMessage)
